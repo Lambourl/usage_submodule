1466915c699a581c916b67cbc0cba081c27458ed
Lambourl <lmbrluc@gmail.com>
2022-10-31 09:16:08 +0100

    :wrench: add submodules

---
+++ main_base.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <example_of_submodule/help.hpp>
+
+int main(){
+    std::cout << "simple example"<<std::endl;
+    display_an_message("example of sub");
+    return 0;
+}
